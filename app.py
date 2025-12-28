f340a55cda87feddc21b3d4959ad369c8a5ed2e9
ryujigohara-png <ryuji.gohara@gmail.com>
2025-12-28 15:05:11 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@
 import streamlit as st
 import requests
 import pandas as pd
-import matplotlib.pyplot as
 import matplotlib.pyplot as plt
 import matplotlib.font_manager as fm
 import urllib.request
