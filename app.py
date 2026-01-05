0112130d93d751f31d7626721a1f9984da7675f2
ryujigohara-png <ryuji.gohara@gmail.com>
2026-01-05 20:14:44 +0900

    Update app.py

--- app.py
+++ app.py
@@ -696,9 +696,7 @@ def show_sidebar_controls():
         st.session_state.get("ratios", CONFIG["DEFAULT_RATIOS"]),
         st.session_state.get("show_wind", True),
         st.session_state.get("show_temp", True),
-        st.session_state.get("show_tide", True),
-        st.session_state.get("show_w_text", True),
-        st.session_state.get("show_dir_name", True)
+        st.session_state.get("show_tide", False)
     )
 
     design_params = {
