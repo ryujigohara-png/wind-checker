14dbe6c382e78f3d8da5b1a62165449c1c06ddfa
ryujigohara-png <ryuji.gohara@gmail.com>
2026-01-16 22:16:49 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1946,7 +1946,7 @@ def main():
     import os
     icon_path = "pin_weather_01.png"
     if os.path.exists(icon_path):
-        st.image(icon_path, width=400) 
+        st.image(icon_path, width=500) 
     else:
         st.title("⛵Pin_Weather!")            
        
