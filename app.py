d79f9680602777716f868ad4fcabf3efc50f3dc7
ryujigohara-png <ryuji.gohara@gmail.com>
2026-01-28 20:16:21 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2421,8 +2421,8 @@ def render_compact_control_panel(basho_name):
             /* 垂直方向のブロック間隔を極限まで詰める */
             [data-testid="stVerticalBlock"] > div {
                 padding: 0px !important;
-                margin-top: -0px !important;
-                margin-bottom: -2px !important;
+                margin-top: -1px !important;
+                margin-bottom: -1px !important;
             }
             /* カラム間の隙間 */
             [data-testid="stHorizontalBlock"] {
